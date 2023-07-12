8cf9f40709fc6273e7230805214a1e8f10b5ccd2
zynmurel <comingues99@gmail.com>
2023-07-12 12:18:21 +0800

    make login page components

--- src/app/login/page.js
+++ src/app/login/page.js
@@ -2,7 +2,8 @@
 
 import ThemeProvider from "@/theme/themeProvider";
 import { LockOutlined, UserOutlined } from "@ant-design/icons";
-import { Button, Card, Form, Input } from "antd";
+import { Card, Form, Input, Button, Checkbox } from "antd";
+
 import theme from "./theme/theme.json";
 
 const App = () => {
@@ -31,7 +32,73 @@ const App = () => {
 
   return (
     <ThemeProvider theme={theme}>
-      <Card>Login Page</Card>
+      <div className=" bg-white w-full h-screen flex justify-center items-center">
+        <Card
+          style={{ width: 300 }}
+          bodyStyle={{ height: "100%" }}
+          className=" drop-shadow-xl"
+        >
+          <p className=" text-2xl w-full text-center font-bold m-0">LOGIN</p>
+
+          <Form
+            name="normal_login"
+            className="login-form flex flex-col justify-center items-center"
+            initialValues={{
+              remember: true,
+            }}
+            onFinish={onFinish}
+          >
+            <Form.Item
+              labelCol={{ span: 24 }}
+              className=" mb-0"
+              label="Username"
+              name="username"
+              rules={[
+                {
+                  required: true,
+                  message: "Please input your Username!",
+                },
+              ]}
+            >
+              <Input
+                prefix={<UserOutlined className="site-form-item-icon w-full" />}
+                placeholder="Username"
+                size="large"
+              />
+            </Form.Item>
+            <Form.Item
+              labelCol={{ span: 24 }}
+              className=" mb-5"
+              label="Password"
+              name="password"
+              rules={[
+                {
+                  required: true,
+                  message: "Please input your Password!",
+                },
+              ]}
+            >
+              <Input
+                prefix={<LockOutlined className="site-form-item-icon" />}
+                type="password"
+                placeholder="Password"
+                size="large"
+              />
+            </Form.Item>
+            <Button
+              type="primary"
+              htmlType="submit"
+              className="login-form-button w-full mb-2"
+              size="large"
+            >
+              Log in
+            </Button>
+            <a className="login-form-forgot" href="">
+              Forgot password
+            </a>
+          </Form>
+        </Card>
+      </div>
     </ThemeProvider>
   );
 };
---
+++ src/app/page.js
@@ -0,0 +1,5 @@
+const Page = () => {
+  return <div>Sean</div>;
+};
+
+export default Page;
